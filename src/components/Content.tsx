8bf8d61536be566e974a6835b88a9565167dede0
Eduardo Cano <e.maximocano@gmail.com>
2021-07-17 18:06:27 -0300

    complete

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,22 @@
-export function Content() {
+import { MovieCard } from "./MovieCard"
+
+import '../styles/content.scss';
+
+export function Content({movies, selectedGenre}) {
   // Complete aqui
+  return(
+    <>
+      <header>
+            <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+          </header>
+
+          <main>
+            <div className="movies-list">
+              {movies.map(movie => (
+                <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+              ))}
+            </div>
+          </main>
+    </>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,24 @@
-export function SideBar() {
+import { Button } from "./Button"
+
+import '../styles/sidebar.scss';
+
+export function SideBar({genres, selectedGenreId, handleClickButton}) {
   // Complete aqui
+  return(
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+  
+          <div className="buttons-container">
+            {genres.map(genre => (
+              <Button
+                key={String(genre.id)}
+                title={genre.title}
+                iconName={genre.name}
+                onClick={() => handleClickButton(genre.id)}
+                selected={selectedGenreId === genre.id}
+              />
+            ))}
+          </div>
+      </nav>
+  )
 }
